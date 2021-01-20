9f924fc1cc581b46aad7e388911d59e30693c303
渡久地太一 <toguchitaichi@toguchitaichi-no-MacBook-Pro.local>
2021-01-20 15:41:02 +0900

    最初のコミットです

---
+++ todoproject/todo/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
+from .models import ToDoModel
+from django.urls import reverse_lazy
+
+class TodoList(ListView):
+    template_name = 'list.html'
+    model = ToDoModel
+
+
+class TodoDetail(DetailView):
+    template_name = 'detail.html'
+    model = ToDoModel
+
+class TodoCreate(CreateView):
+    template_name = 'create.html'
+    model = ToDoModel
+    fields = ('title', 'memo', 'priority', 'duedate')
+    success_url = reverse_lazy('list')
+
+class TodoDelete(DeleteView):
+    template_name = 'delete.html'
+    model = ToDoModel
+    success_url = reverse_lazy('list')
+
+class TodoUpdate(UpdateView):
+    template_name = 'update.html'
+    model = ToDoModel
+    success_url = reverse_lazy('list')
+    fields = ('title', 'memo', 'priority', 'duedate')
+
+
+
